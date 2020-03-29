e95d6ab917b3faab24ee323bc5fbd6f521d18c2c
magorors <magorors_kollo@yahoo.com>
2020-03-30 02:55:44 +0300

    Improved version

--- BFMC_v09/src/data/LaneDetector.py
+++ BFMC_v09/src/data/LaneDetector.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import imutils
+import time
 from threading import Thread
 
 
@@ -14,57 +15,7 @@ class LaneDetector: #Thread
         self.inP = inP
         self.outP = outP
 
-    def region_of_interest(self, local_img):
 
-        """
-        Takes an image and returns a mask with the region of interest
-        local_img should be gray
-        The region of interest is composed by a triangle and a rectangle
-        in the bottom half of the image
-        Because we don`t want any detections to be made in the middle of the road
-        we used a black triangle to cover that part
-        """
-
-        # take the dimensions of the image
-        height = local_img.shape[0]
-        width = local_img.shape[1]
-
-        # create a black mask
-        local_mask = np.zeros_like(local_img)
-
-        # the triangle used for the mask
-        # triangle1 = np.array([[(0, int(0.7*height)), (width, int(0.7*height)), (int(width/2), int(0.45*height))]])
-        # cv2.fillPoly(local_mask, triangle1, 255)
-
-        # the rectangle used for the mask
-        # rectangle = np.array([[(0, height), (0, int(0.7*height)), (width, int(0.7*height)), (width, height)]])
-        # cv2.fillPoly(local_mask, rectangle, 255)
-        
-        rectangle = np.array([[(0, height), (0, int(0.4*height)), (width, int(0.4*height)), (width, height)]])
-        cv2.fillPoly(local_mask, rectangle, 255)
-        
-        # the triangle used for blacking out the middle part
-       # triangle2 = np.array([[(int(0.2*width), height), (int(0.8*width), height), (int(width/2), int(0.6*height))]])
-        #cv2.fillPoly(local_mask, triangle2, 0)
-
-        #cv2.imshow('test01', local_mask)          # TO BE DELETED!!!!!!!!!!
-        return local_mask
-
-    def get_canny(self, local_img):
-
-        """
-        Takes an image and returns the canny of that image
-        First -> change the image to gray
-        Second -> apply a Gaussian Blur on the image to decrease the noise
-        Then -> apply the canny edge detection function on the image
-        Last -> apply the mask to the canny image
-        """
-        gauss = cv2.GaussianBlur(local_img, (5, 5), 0)
-        local_canny = cv2.Canny(gauss, 100, 200)
-        local_mask = self.region_of_interest(local_img)
-        #cv2.imshow('test3', local_mask)          # TO BE DELETED!!!!!!!!!!
-        #return cv2.bitwise_and(local_canny, local_mask)  # apply the mask to the canny image
-        return local_canny   # when the camera is straiight down
     def make_coordinates(self, local_line, im_shape):
         """
         This function calculates the coordinates based on the slope and the y intercept
@@ -93,9 +44,9 @@ class LaneDetector: #Thread
         Last -> find the average slope for each category and compute the end points for the average line
         Return a numpy array which contains the 2 lanes (right and left)
         """
+        gauss = cv2.GaussianBlur(local_img, (5, 5), 0)
+        local_img = cv2.Canny(gauss, 100, 200)
         detected_lines = cv2.HoughLinesP(local_img, 2, np.pi / 180, 100, np.array([]), minLineLength=20, maxLineGap=5)
-        #print(detected_lines)           # TO BE DELETED !!!!!!!!!!!!!!
-        #print(type(detected_lines))     # TO BE DELETED !!!!!!!!!!!!!!
         right_fit = []
         left_fit = []
         height, width = local_img.shape
@@ -113,11 +64,12 @@ class LaneDetector: #Thread
             parameters = np.polyfit((x1, x2), (y1, y2), 1)  # finds a poly of degree 1 that passes through the 2 points
             slope = parameters[0]
             intercept = parameters[1]
-            if slope > 0 and x1 > width/2 and x2 > width/2:
+            if slope > 0 and x1 > width/1.75 and x2 > width/1.75:
                 right_fit.append((slope, intercept))
-            elif slope < 0 and x1 < width/2 and x2 < width/2:
+            elif slope < 0 and x1 < width/1.75 and x2 < width/1.75:
                 left_fit.append((slope, intercept))
             else:
+
                 continue  # we don`t care about the lines that don`t fall under those two categories
 
         if right_fit:
@@ -143,21 +95,21 @@ class LaneDetector: #Thread
             l = right_lane
             try:
                 cv2.line(auxiliary_img, (l[0], l[1]), (l[2], l[3]), (255, 255, 255), 20)
-            except OverflowError:
+            except (OverflowError,TypeError):
                 print("OverflowError Right")
 
         if left_lane is not None:
             l = left_lane
             try:
                 cv2.line(auxiliary_img, (l[0], l[1]), (l[2], l[3]), (255, 255, 255), 20)
-            except OverflowError:
+            except (OverflowError,TypeError):
                 print("OverflowError Left")
 
         return cv2.addWeighted(local_img, 0.6, auxiliary_img, 1, 1)
 
     def if_horizontal(self, local_img):
         x,y,_ = local_img.shape
-        local_img = local_img[x-700:x-200,:]
+        local_img = local_img[x-700:x-200,y-1200:y-200]
         gray = cv2.cvtColor(local_img,cv2.COLOR_BGR2GRAY)
         gray = cv2.bitwise_not(gray)
         bw = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,  cv2.THRESH_BINARY, 15, -2)
@@ -175,43 +127,79 @@ class LaneDetector: #Thread
                 box = np.int0(box)
                 cv2.drawContours(local_img,[cnt],0,(0,255,0),2)
                 #cv2.drawContours(horizontal,[cnt],0,255,-1)
-                cv2.putText(local_img, "HORIZONTAL LINE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                #cv2.putText(local_img, "HORIZONTAL LINE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 return True
-            else:
-                cv2.putText(local_img, "NO HORIZONTAL LINE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                return False
-
-
+        if not contours:
+            #cv2.putText(local_img, " NO HORIZONTAL LINE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            return False
+            
+    # def birdseye(self, local_img):
+    #     IMAGE_H = 300
+    #     IMAGE_W = 1280
+    #     src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
+    #     dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
+    #     M = cv2.getPerspectiveTransform(src, dst)
+    #     Minv = cv2.getPerspectiveTransform(dst, src)
+    #     img = local_img
+    #     img = img[550:(550+IMAGE_H), 0:IMAGE_W]
+    #     warped_img = cv2.warpPerspective(img, M, (IMAGE_W, IMAGE_H))
+    #     return warped_img
 
     def run(self):
         """
 
         """
-        #cap = cv2.VideoCapture('/home/mgrrr/Documents/dataset/Records/Set1/c1.mp4')  #TO BE DELETED!!!
+        cap = cv2.VideoCapture('/home/mgrrr/Documents/dataset/Records/Set1/10.h264')  #TO BE DELETED!!!
 
         #frame_copy = cv2.imread('D:\\Cristi\\BoschFutureMobility\\Records\\testCurba4.png')
-
+        ts = [0, 0 , False]
+        elapsed = 0
+        
         while True:
-            data = self.inP.recv()
-            frame_copy = data[1]
+            #data = self.inP.recv()
+            # frame_copy = data[1]
+            hor_line = False
+            elapsed = 0
             _, frame_copy = cap.read()
             #frame_copy = imutils.rotate(frame_copy,270)
-            lane_coordinates = self.get_lines_coordinates(self.get_canny(frame_copy))
+            x,y,_ = frame_copy.shape
             hline = self.if_horizontal(frame_copy)
-
-
-            
+            mview = frame_copy[x-700:x-200, y-1200:y-200]
+            x = int(x * 50/100) 
+            y = int(y * 50/100)
+            dim = (x,y)
+            frame_copy = cv2.resize(frame_copy, dim, interpolation = cv2.INTER_AREA)
+            lane_coordinates = self.get_lines_coordinates(frame_copy)
+            display_lane = self.display_lines(frame_copy, lane_coordinates)
+            #hline = self.if_horizontal(frame)
+            if hline == True and ts[2] == False:
+                ts[0] = time.time()
+                ts[2] = True
+            elif hline == True and ts[2] == True:
+                ts[1] = time.time()
+                elapsed = ts[1] - ts[0]
+            if hline == False and ts[2] == True:
+                ts[1] = time.time()
+                elapsed = ts[1] - ts[0]
+                ts = [0,0, False]
+            if elapsed > 1.5:
+                cv2.putText(display_lane, "HORIZONTAL LINE", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(display_lane, "ELAPSED" + str(elapsed), (40, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                elapsed = 0
+                hor_line = True
             #print('LaneDetector, lane_coord: ' + str(lane_coordinates)) #TO BE DELETED
             #self.outP.send((lane_coordinates, frame_copy.shape))
-            
+            # x,y,_ = frame_copy.shape
+            # mview = frame[x-700:x-200, y-1200:y-200]
             #cv2.imshow('test1', frame_copy)
-            display_lane = self.display_lines(frame_copy, lane_coordinates)
+            #display_lane = self.display_lines(frame_copy, lane_coordinates)
             cv2.imshow('test2', display_lane)
+            cv2.imshow("testM", mview)
 
             if cv2.waitKey(1) == 27:
                 break
 
         cv2.destroyAllWindows()
 
-# l1 = LaneDetector(2,3)
-# l1.run()
+l1 = LaneDetector(2,3)
+l1.run()
